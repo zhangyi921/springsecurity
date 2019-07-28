2970101d86b592dd084e2b2ce215538c04f8d71d
Yi Zhang <yi16@ualberta.ca>
2019-07-28 15:25:06 -0600

    custome login form

--- src/main/java/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,9 +21,19 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication()
 			.withUser(users.username("1").password("1").roles("employee"))
 			.withUser(users.username("2").password("2").roles("manager"))
-			.withUser(users.username("31").password("3").roles("admin"));
+			.withUser(users.username("3").password("3").roles("admin"));
 		
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/showMyLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll();
+	}
 	
 }
