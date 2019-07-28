6dc39818f147f1f22e4bc57072b9c3bd5a120b68
Yi Zhang <yi16@ualberta.ca>
2019-07-28 14:08:04 -0600

    Initial commit

---
+++ src/main/java/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,28 @@
+package springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// add our users for in memory authenticaiton
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("1").password("1").roles("employee"))
+			.withUser(users.username("2").password("2").roles("manager"))
+			.withUser(users.username("31").password("3").roles("admin"));
+		
+	}
+
+	
+}
